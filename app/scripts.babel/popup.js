bc17219f1bb11b2a972a7f27b2d286f95c5a0532
entriol <entriol@outlook.com>
2017-01-23 02:19:34 +0300

    simple GET request to tureng

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,3 +1,24 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Popup');
+document.addEventListener('DOMContentLoaded', ()=>{
+  var request = new XMLHttpRequest();
+  request.open('GET', 'http://tureng.com/search/word', true);
+  request.onreadystatechange = () => {
+    if(request.readyState === 4) {
+      if(request.status === 200) {
+        const data = request.responseText;
+        const response = $(data).find('.searchResultsTable');
+
+        for (let i = 0; i < response.length; i++) {
+          $('#content')
+            .append(response[i].outerHTML);
+        }
+
+      } else {
+        // TODO: error in your conn
+      }
+    }
+  };
+
+  request.send();
+});
